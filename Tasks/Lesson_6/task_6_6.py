7216ac1614db340ab3fe202a14352c751b527de3
Luke <sir.graf@gmail.com>
2023-01-08 22:45:36 +0300

    fix task 6 (lesson 6)

--- Tasks/Lesson_6/task_6_6.py
+++ Tasks/Lesson_6/task_6_6.py
@@ -3,5 +3,5 @@
 
 numbers_lst = [23, 1, 2, 5, 6, 1, 4353, 4, 5, 6, 0]
 
-numbers_lst.sort(key=lambda x: (x % 2 == 0, x % 2 != 0), reverse=True)
+numbers_lst.sort(key=lambda x: x % 2 == 0, reverse=True)
 print(numbers_lst)
