f9158de27075e8b37ddb2211a075fdbb1b3ec30d
Luke <sir.graf@gmail.com>
2023-01-08 23:40:39 +0300

    fix task 7 (lesson 6)

--- Tasks/Lesson_6/task_6_7.py
+++ Tasks/Lesson_6/task_6_7.py
@@ -4,19 +4,26 @@
 
 numbers_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# def neighbours_sum(n_lst):
+#     gen_dict = dict(enumerate(n_lst))
+#     gen_dict[-1] = gen_dict.pop(len(gen_dict) - 1)
+#     sum_lst = []
+#     print(numbers_lst)
+#     def sum_generator():
+#         for i in gen_dict:
+#             yield gen_dict[i + len(gen_dict) - 1 if i == -1 else i - 1] \
+#                 + gen_dict[i - len(gen_dict) + 1 if i == len(gen_dict) - 2 else i + 1]
+#
+#     get_item = sum_generator()
+#     for _ in n_lst:
+#         sum_lst.append(get_item.__next__())
+#     print(sum_lst)
+
 def neighbours_sum(n_lst):
-    gen_dict = dict(enumerate(n_lst))
-    gen_dict[-1] = gen_dict.pop(len(gen_dict) - 1)
     sum_lst = []
     print(numbers_lst)
-    def sum_generator():
-        for i in gen_dict:
-            yield gen_dict[i + len(gen_dict) - 1 if i == -1 else i - 1] \
-                + gen_dict[i - len(gen_dict) + 1 if i == len(gen_dict) - 2 else i + 1]
-
-    get_item = sum_generator()
-    for _ in n_lst:
-        sum_lst.append(get_item.__next__())
+    for i in range(len(n_lst)):
+        sum_lst.append(n_lst[i - 1] + n_lst[i - len(n_lst) + 1])
     print(sum_lst)
 
 neighbours_sum(numbers_lst)
--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print('hello')
+# inp_number = input("N: ")
+#
+# result = 0
+#
+# for i in range(len(inp_number)):
+#     result += int(inp_number[i])
+#
+# print(result)
+
+# list_numbers = [1,2,3,4,5,6,7,82,3,2,2,3,2,34,4,2,32]
+#
+# for i in range(list_numbers.count(2)):
+#     list_numbers.remove(2)
+#
+# print(list_numbers)
+
+# try:
+#     a = int(input())
+#     b = int(input())
+#     c = a / b
+# except ValueError:
+
+# i = -1
+# number = int(input("N: "))
+# while 2**(i + 1) <= number:
+#     i += 1
+# print(i)
+
+# amount = float(input("Amount: "))
+# interest_rate = float(input("IR: "))
+
+#
+# str = '123'
+# custom_iter = iter(str)
+# print(type(custom_iter))
+
+# def foo(str_):
+#     return list(filter(lambda x: len(x) >= 5, str_.split()))
+#
+# print(foo(input("Введите строку: ")))
+
+from random import choice
+
+passwords = {}
+print(choice('abcd'))
+def generate_password(res_name, pass_len):
+    for i in range(pass_len):
+        choice()
