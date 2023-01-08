e4aed37b1338315f8aef07304524b9ca7ce4792c
Luke <sir.graf@gmail.com>
2023-01-08 19:05:33 +0300

    initial and fill in task 7 (lesson 6)

---
+++ Tasks/Lesson_6/task_6_7.py
@@ -0,0 +1,22 @@
+# Дан список чисел, необходимо для каждого элемента посчитать сумму его
+# соседей, для крайних чисел одним из соседей является число с противоположной
+# стороны списка
+
+numbers_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def neighbours_sum(n_lst):
+    gen_dict = dict(enumerate(n_lst))
+    gen_dict[-1] = gen_dict.pop(len(gen_dict) - 1)
+    sum_lst = []
+    print(numbers_lst)
+    def sum_generator():
+        for i in gen_dict:
+            yield gen_dict[i + len(gen_dict) - 1 if i == -1 else i - 1] \
+                + gen_dict[i - len(gen_dict) + 1 if i == len(gen_dict) - 2 else i + 1]
+
+    get_item = sum_generator()
+    for _ in n_lst:
+        sum_lst.append(get_item.__next__())
+    print(sum_lst)
+
+neighbours_sum(numbers_lst)
