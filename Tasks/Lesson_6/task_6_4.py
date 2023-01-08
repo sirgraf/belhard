a8fa0effc785cab8db7fd2242d4e9a5e9650d83d
Luke <sir.graf@gmail.com>
2023-01-08 16:48:48 +0300

    initial and fill in task 4 (lesson 6)

---
+++ Tasks/Lesson_6/task_6_4.py
@@ -0,0 +1,6 @@
+objects_lst = [1, 3, True, "string", 1.3, False, (3, 4), "string2", ["stringinlist"], 3]
+
+def lst_filter(target_list):
+    return list(filter(lambda x: isinstance(x, str), target_list))
+
+print(f"Sorted list: {lst_filter(objects_lst)}")
